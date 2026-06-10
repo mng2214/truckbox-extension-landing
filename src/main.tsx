cb5632fadf1d60df98675380d19020798a1c2ed8
mng2214 <mng2214@gmail.com>
2026-06-10 08:35:47 -0500

    clean up

--- src/main.tsx
+++ src/main.tsx
@@ -5,7 +5,6 @@ import "./styles.css";
 import App, { SmoothScroll } from "./App";
 import PrivacyPage from "./pages/Privacy";
 import GuidePage from "./pages/Guide";
-import DatExample from "./pages/DatExample";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
@@ -15,7 +14,6 @@ ReactDOM.createRoot(document.getElementById("root")!).render(
         <Route path="/" element={<App />} />
         <Route path="/privacy" element={<PrivacyPage />} />
         <Route path="/guide" element={<GuidePage />} />
-        <Route path="/dat" element={<DatExample />} />
         <Route path="*" element={<App />} />
       </Routes>
     </BrowserRouter>
